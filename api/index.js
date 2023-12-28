c0ca5e5c4dee036d48b95a495ca1e8b0280982d8
mcapxx <chetanmahesh1620@gmail.com>
2023-12-28 20:34:53 +0530

    signup page completed data saved to mongodb

--- api/index.js
+++ api/index.js
@@ -34,7 +34,7 @@ app.use('/api/auth',authRoutes)
 
 app.use((err,req,res,next)=>{
     const statusCode = err.statusCode || 500;
-    const message = err.message || "internal server error"
+    const message = err.message || "internal  error"
 
     return res.status(statusCode).json({
         success:false,
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,23 +1,90 @@
-import {Link} from 'react-router-dom'
-
+import { Link } from "react-router-dom";
+import { useState,useRef } from "react";
 function Signup() {
+  const [error, setError] = useState(false);
+  const [loading,setLoading] = useState(false);
+  
+  const UsernameElement = useRef();
+  const EmailElement = useRef();
+  const PasswordElement = useRef();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const name = UsernameElement.current.value;
+    const email = EmailElement.current.value;
+    const password = PasswordElement.current.value;
+
+    const newuser = { name, email, password };
+
+
+    try {
+      setLoading(true);
+      setError(false);
+      const res = await fetch("/api/auth/singup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newuser),
+      });
+      const data = await res.json();
+      setLoading(false)
+
+      UsernameElement.current.value = "";
+      EmailElement.current.value = '';
+      PasswordElement.current.value = '';
+
+      if(data.success===false){
+        setError(true)
+        return;
+      }
+      console.log(data);
+    } catch (error) {
+      setLoading(false)
+      console.error("Error during signup:", error);
+    }
+  };
+
+  // console.log(formData);
+
   return (
-    <div className='p-3 max-w-lg mx-auto'>
-     <h1 className='text-3xl text-center font-semibold my-7'>Signup</h1> 
-     <form className='flex flex-col gap-4'>
-      <input type='text' placeholder='Username' id='username' className='bg-slate-100 p-3 rounded-lg'/>
-      <input type='email' placeholder='Email' id='email' className='bg-slate-100 p-3 rounded-lg'/>
-      <input type='password' placeholder='Password' id='password' className='bg-slate-100 p-3 rounded-lg'/>
-      <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>Sign up</button>
-     </form>
-     <div className='flex gap-2 mt-5'>
-      <p>Have an Account?</p>
-      <Link to="/signin">
-      <span className='text-blue-500'>Sign in</span>
-      </Link>
-     </div>
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">Signup</h1>
+      <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Username"
+          id="username"
+          className="bg-slate-100 p-3 rounded-lg"
+          ref={UsernameElement}
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          id="email"
+          className="bg-slate-100 p-3 rounded-lg"
+          ref={EmailElement}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          id="password"
+          className="bg-slate-100 p-3 rounded-lg"
+          ref={PasswordElement}
+        />
+        <button disabled={loading} className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
+          {loading ? 'Loading...' : 'Sign Up'}
+        </button>
+      </form>
+      <div className="flex gap-2 mt-5">
+        <p>Have an Account?</p>
+        <Link to="/signin">
+          <span className="text-blue-500">Sign in</span>
+        </Link>
+      </div>
+      <p className="text-red-700 mt-5">{error && 'Error during signup'}</p>
     </div>
-  )
+  );
 }
 
-export default Signup
+export default Signup;
