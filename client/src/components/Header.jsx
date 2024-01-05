4a38e59180e4f0fe342d8393d596aa5f3745fa28
mcapxx <chetanmahesh1620@gmail.com>
2024-01-05 23:35:13 +0530

    fixed bug

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -5,7 +5,7 @@ function Header() {
   const {currentUser} = useSelector(state => state.user) 
   return (
     <div className='bg-slate-200'>
-      <div className='flex justify-between items-center mx-10 p-5 '>
+      <div className='flex justify-between items-center max-w-6xl mx-auto p-3 '>
         <h1 className='font-bold'><Link to={'/'}>MY APP</Link></h1>
         <ul className='flex gap-4'>
            <Link to={'/'}><li>Home</li></Link>
