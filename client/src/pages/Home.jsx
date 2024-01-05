590ea12da31f7df7d3fbb05af5b4662513a386b1
mcapxx <chetanmahesh1620@gmail.com>
2024-01-05 23:38:57 +0530

    bg color changed

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Home() {
   return (
-    <div className='px-4 py-12 max-w-2xl mx-auto'>
+    <div className='px-4 py-12 max-w-2xl mx-auto bg-zinc-500 text-white'>
       <h1 className='text-3xl font-bold mb-4 text-slate-800'>Welcome to my Auth App!</h1>
       <p className='mb-4 text-slate-700'>
         This is a full-stack web application built with the MERN (MongoDB,
